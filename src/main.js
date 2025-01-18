1d922763a657c06062a3d34fe3e353b42d0f7d37
Dimitris-Toulis <dimitritouli@gmail.com>
2025-01-18 17:01:11 +0200

    Optimizations and fixes

--- src/main.js
+++ src/main.js
@@ -1,84 +1,75 @@
 import "./style.css";
-function showModal(p) {
-	const modal = document.getElementById("modal");
-	const close = document.getElementById("close");
-	modal.className = "p"+p;
+let getColor = (p) => (p ? "#1e90ff" : "#eb0000");
+let showModal = (p) => {
+	let modal = document.querySelector("#m");
+	let t = document.querySelector("p");
+	t.innerText = `Player ${p + 1} has won!`;
+	t.style.color = getColor(!p);
+	modal.style.setProperty("--s", getColor(p));
+	modal.style.setProperty("--m", getColor(!p));
 	modal.showModal();
-	close.onclick = ()=>modal.close();
-	window.onclick = (e) => {
-		if (e.target == modal) {
-			modal.close()
-		}
+	let close = () => {
+		modal.close();
+		reset();
 	};
-}
+	document.querySelector("#c").onclick = close;
+	window.onclick = (e) => e.target == modal && close();
+};
 
-let player,par,grid;
-let gridGet = (i,j) => grid[11*i+j];
-	
-let xfind = (a) => (par[a] == a ? a : xfind(par[a]));
+let player, par, grid;
+
+let xfind = (a) => (par[a] == a ? a : (par[a] = xfind(par[a])));
 let union = (a, b) => (par[xfind(a)] = xfind(b));
 for (let i = 0; i < 11; i++) {
-	const line = document.createElement("div");
-	line.style.setProperty("--i", i.toString());
-	line.className = "rhombus-line";
+	let line = document.createElement("div");
+	line.style.cssText = "--i:" + i;
+	line.className = "l";
 	for (let j = 0; j < 11; j++) {
-		const hexagon = document.createElement("div");
-		hexagon.className = "hexagon";
-		hexagon.addEventListener(
-			"click",
-			(e) => {
-				if(gridGet(i,j) != null) return;
-				e.target.style.setProperty(
-					"background-color",
-					player ? "#1e90ff" : "#eb0000"
-				);
-				grid[11 * i + j] = player;
-				if (j == 0 && !player) union(123, 11 * i + j);
-				if (j == 10 && !player) union(124, 11 * i + j);
-				if (i == 0 && player) union(121, 11 * i + j);
-				if (i == 10 && player) union(122, 11 * i + j);
-				for (let d of [
-					[-1, 0],
-					[-1, 1],
-					[0, -1],
-					[0, 1],
-					[1, 0],
-					[1, -1],
-				]) {
-					let ni = i + d[0],
-						nj = j + d[1];
-					if (
-						ni >= 0 &&
-						nj >= 0 &&
-						ni < 11 &&
-						nj < 11 &&
-						gridGet((i + d[0]), j + d[1]) === player
-					) {
-						union(11 * i + j, 11 * (i + d[0]) + j + d[1]);
-					}
+		let hexagon = document.createElement("div");
+		hexagon.className = "h";
+		let id = 11 * i + j;
+		hexagon.onclick = () => {
+			if (grid[id] != undefined) return;
+			hexagon.style.cssText = "background:" + getColor(player);
+			grid[id] = player;
+			if (j == 0 && !player) union(121, id);
+			if (j == 10 && !player) union(122, id);
+			if (i == 0 && player) union(123, id);
+			if (i == 10 && player) union(124, id);
+			for (let [a, b] of [
+				[-1, 0],
+				[-1, 1],
+				[0, -1],
+				[0, 1],
+				[1, 0],
+				[1, -1],
+			]) {
+				let ni = i + a,
+					nj = j + b;
+				let n = 11 * ni + nj;
+				if (ni >= 0 && nj >= 0 && ni < 11 && nj < 11 && grid[n] === player) {
+					union(id, n);
 				}
-				if (xfind(121) == xfind(122)) {
-					showModal(1);
-				} else if (xfind(123) == xfind(124)) {
-					showModal(2);
-				}
-				player = !player;
 			}
-		);
+			if (xfind(121) == xfind(122)) {
+				showModal(1);
+			} else if (xfind(123) == xfind(124)) {
+				showModal(0);
+			}
+			player = 1 - player;
+		};
 		line.appendChild(hexagon);
 	}
-	document.getElementById("board").appendChild(line);
+	document.body.appendChild(line);
 }
-function reset(){
-	player = true;
-	par = Array(11 * 11 + 4)
-	.fill(0)
-	.map((_, i) => i);
-	grid = Array(11 * 11).fill(null);
-	for(let elem of document.getElementsByClassName("hexagon")) {
-		elem.style.removeProperty("background-color");
+function reset() {
+	player = 1;
+	par = Array(125)
+		.fill(0)
+		.map((_, i) => i);
+	grid = Array(121);
+	for (let elem of document.querySelectorAll(".h")) {
+		elem.style.cssText = "";
 	}
 }
-document.getElementById("reset").addEventListener("click",reset);
 reset();
-window.a = showModal;
---
+++ vite.config.js
@@ -0,0 +1,53 @@
+import { viteSingleFile } from "vite-plugin-singlefile";
+import { ViteMinifyPlugin } from "vite-plugin-minify";
+import { writeFile } from "fs/promises";
+import qrcode from "qrcode";
+
+/** @type {import('vite').UserConfig} */
+export default {
+	plugins: [
+		viteSingleFile(),
+		ViteMinifyPlugin({
+			collapseBooleanAttributes: true,
+			removeAttributeQuotes: true,
+			removeEmptyAttributes: true,
+			removeRedundantAttributes: true,
+		}),
+		{
+			name: "remove-crossorigin-attributes",
+			apply: "build",
+			order: "post",
+			transformIndexHtml(html) {
+				return html.replaceAll(/ crossorigin/g, "");
+			},
+		},
+		{
+			name: "base64-encode",
+			apply: "build",
+			order: "post",
+			async writeBundle(_, bundle) {
+				const datauri =
+					"data:text/html;charset=utf-8;base64," +
+					Buffer.from(bundle["index.html"].source, "utf-8").toString(
+						"base64url"
+					);
+				await writeFile("./dist/datauri.txt", datauri);
+				await qrcode.toFile("./dist/qrcode.png", datauri, {
+					errorCorrectionLevel: "L",
+					version: 40
+				});
+			},
+		},
+	],
+	build: {
+		modulePreload: false,
+		cssMinify: "lightningcss",
+		minify: "terser",
+		terserOptions: {
+			module: true,
+			compress: {
+				passes: 3
+			}
+		},
+	},
+};
